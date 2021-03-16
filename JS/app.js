4751d30e2b26c59f1b33ea3b3dec57f354eaf41c
Ryan Mallory <ryanandmallory@yahoo.com>
2021-03-15 21:23:39 -0400

    create select, quote and layout pages

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,15 @@ window.onload = function(){
     document.querySelector('video').play();
 }
 
+document.addEventListener('click', function (event) {
+	if (event.target.classList.contains('theme-image')){
+        document.querySelectorAll('.theme-image').forEach((image) => {
+            image.style.border = 'none';
+            let clickedElem = event.target;
+            clickedElem.style.border = '3px solid #e3e3e3';
+            themeImage =  clickedElem.alt;
+            console.log('Theme name ' + themeImage);
+        });
+    }
+}, false);
+
